7c7549be9a8bb9d18a74d74d1a48a0856d20c2af
lakompe <akbar.muhammad.me@gmail.com>
2026-02-05 09:12:42 +0800

    Enable ccache for faster BP builds
    Add ccache support to makemacos.sh:
    - Check if ccache is available
    - Set CC/CXX to use ccache wrapper
    - Print ccache version on build start
    
    This significantly speeds up rebuilds by caching compiled objects.

--- makemacos.sh
+++ makemacos.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -x
 
+# CBV: Enable ccache for faster rebuilds
+if which ccache >/dev/null 2>&1; then
+  export CC="ccache cc"
+  export CXX="ccache c++"
+  echo "ccache enabled: $(ccache --version | head -1)"
+fi
+
 #brew install sdl2
 export G=`git rev-list HEAD --count`
 echo $G
